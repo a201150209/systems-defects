b767197c9432bc761131a8c0a157f5041cdc69e9
Dmitriy <a20150209@gmail.com>
2020-08-21 17:14:41 +0300

    фильтры и график разнесены по отдельным модулям

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
+import {setDefectsByMonth} from './filters.js';
 import {renderChart} from './chart.js';
 import {renderTable} from './table.js';
 
+setDefectsByMonth();
 renderChart();
 renderTable();
